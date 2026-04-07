ca2108c01eabf08f520a3c3e90a6a18c2cb48369
manos <m.psanis@portbase.com>
2026-04-07 15:14:14 +0200

    add docker-compose file

--- src/main/java/com/example/app/user/api/query/GetUserProfile.java
+++ src/main/java/com/example/app/user/api/query/GetUserProfile.java
@@ -7,12 +7,15 @@ import io.fluxzero.sdk.tracking.handling.HandleQuery;
 import io.fluxzero.sdk.tracking.handling.Request;
 import jakarta.annotation.Nullable;
 import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
 
 @Value
+@Slf4j
 public class GetUserProfile implements Request<UserProfile> {
 
     @HandleQuery
     UserProfile handle(@Nullable Sender sender) {
+        log.info("Getting user profile for {}", sender);
         return sender == null ? null : Fluxzero.loadAggregate(sender.getUserId()).get();
     }
 }
