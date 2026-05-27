85ac73d053ff24be83c7b601d3f0006a0888727b
manos <m.psanis@portbase.com>
2026-05-27 10:39:28 +0200

    store token

--- src/main/java/com/example/app/user/authentication/GetKeycloakToken.java
+++ src/main/java/com/example/app/user/authentication/GetKeycloakToken.java
@@ -1,37 +1,22 @@
 package com.example.app.user.authentication;
 
-import com.example.app.refdata.api.query.SendWebRequest;
-import io.fluxzero.sdk.tracking.Consumer;
-import io.fluxzero.sdk.tracking.ForeverRetryingErrorHandler;
+import io.fluxzero.common.MemoizingSupplier;
+import io.fluxzero.sdk.Fluxzero;
+import io.fluxzero.sdk.common.ClientUtils;
+import io.fluxzero.sdk.tracking.handling.HandleQuery;
 import io.fluxzero.sdk.tracking.handling.Request;
-import io.fluxzero.sdk.web.HttpRequestMethod;
-import io.fluxzero.sdk.web.WebRequest;
-import io.fluxzero.sdk.web.WebResponse;
-import lombok.SneakyThrows;
 
-import static io.fluxzero.common.api.Metadata.objectMapper;
-import static io.fluxzero.sdk.configuration.ApplicationProperties.requireProperty;
+import java.time.Duration;
 
-@Consumer(name = "keycloak-consumer", errorHandler = ForeverRetryingErrorHandler.class)
-public class GetKeycloakToken extends SendWebRequest implements Request<String> {
-    @Override
-    protected WebRequest.Builder buildRequest(WebRequest.Builder requestBuilder, Sender sender) {
-        String body = "grant_type=client_credentials"
-                + "&client_id=" +requireProperty("keycloak.clientId")
-                + "&client_secret=" + requireProperty("keycloak.clientSecret");
-        return requestBuilder
-                .acceptGzipEncoding(false)
-                .url("%s/realms/%s/protocol/openid-connect/token".formatted(requireProperty("keycloak.baseUrl"), requireProperty("keycloak.realm")))
-                .method(HttpRequestMethod.POST)
-                .contentType("application/x-www-form-urlencoded")
-                .body(body);
+public class GetKeycloakToken implements Request<String> {
+    MemoizingSupplier<String> memoizedToken = ClientUtils.memoize(this::fetchToken, Duration.ofSeconds(300));
+
+    private String fetchToken() {
+        return Fluxzero.queryAndWait(new GetKeycloakTokenProxy());
     }
 
-    @SneakyThrows
-    @Override
-    protected String handleResponse(WebResponse response, WebRequest request) {
-        Object responseBody = super.handleResponse(response, request);
-        return objectMapper.readTree(responseBody.toString()).get("access_token").asText();
+    @HandleQuery
+    String token() {
+        return memoizedToken.get();
     }
-
 }
